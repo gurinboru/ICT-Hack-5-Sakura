4ebb860ac3c08369ca5b0744c1154864adee9286
Tereshchenko Vlad <ddiavall@yandex.ru>
2022-12-10 01:43:05 +0300

    [Feat]: added students page & some modifies

--- start/views.py
+++ start/views.py
@@ -1,5 +1,17 @@
 import os
+from django.contrib.auth.decorators import login_required
+from .models import *
 from django.shortcuts import render, redirect
 
 def start(request):
+    if (request.user.is_authenticated):
+        return redirect('/students')
     return render(request, 'start/start.html')
+
+@login_required(login_url='/login')
+def getCandidates(request):
+    students = Student.objects.all()
+    content = {
+        "students" : students
+    }
+    return render(request, 'start/students.html',content)
