e9b2b0790883c482869cbbc1143788a207a5169c
UntilaAA <untila.a.a@mail.ru>
2022-12-11 00:31:46 +0300

    [Fix]: add project page style added

--- start/forms.py
+++ start/forms.py
@@ -21,33 +21,33 @@ class changeStudentForm(forms.Form):
                        required=False)
 
 class addProjectForm(forms.Form):
-    name = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control", "placeholder": "01.01.1999"}),
+    name = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control", "placeholder": "Название..."}),
                        required=True)
-    definitions = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control", "placeholder": "01.01.1999"}),
+    definitions = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control", "placeholder": "Описание..."}),
                        required=True)
-    budjet = forms.IntegerField(widget=forms.NumberInput(attrs={"class": "form-control", "placeholder": "01.01.1999"}),
+    budjet = forms.IntegerField(widget=forms.NumberInput(attrs={"class": "form-control1", "placeholder": "Бюджет..."}),
                        required=True)
-    dedlines = forms.IntegerField(widget=forms.NumberInput(attrs={"class": "form-control", "placeholder": "01.01.1999"}),
+    dedlines = forms.IntegerField(widget=forms.NumberInput(attrs={"class": "form-control1", "placeholder": "Сроки..."}),
                        required=True)
-    positions = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control", "placeholder": "01.01.1999"}),
+    positions = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control", "placeholder": "Должности..."}),
                        required=True)
-    techDocument = forms.FileField(widget=forms.FileInput(attrs={"class": "form-control", "placeholder": "01.01.1999"}),
+    techDocument = forms.FileField(widget=forms.FileInput(attrs={"class": "form-control", "placeholder": "ТЗ..."}),
                        required=True)
-    goalOfProject = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control", "placeholder": "01.01.1999"}),
+    goalOfProject = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control", "placeholder": "Цель..."}),
                        required=True)
-    background = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control", "placeholder": "01.01.1999"}),
+    background = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control", "placeholder": "Опыт..."}),
                        required=True)
-    result = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control", "placeholder": "01.01.1999"}),
+    result = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control", "placeholder": "Результат..."}),
                        required=True)
-    criterias = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control", "placeholder": "01.01.1999"}),
+    criterias = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control", "placeholder": "Критерии..."}),
                        required=True)
-    tags = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control", "placeholder": "01.01.1999"}),
+    tags = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control", "placeholder": "Теги..."}),
                        required=False)
-    contactPersonName = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control", "placeholder": "01.01.1999"}),
+    contactPersonName = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control", "placeholder": "ФИО..."}),
                        required=True)
-    contactPersonPhone = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control", "placeholder": "01.01.1999"}),
+    contactPersonPhone = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control", "placeholder": "Телефон..."}),
                        required=True)
-    contactPersonEmail = forms.EmailField(widget=forms.EmailInput(attrs={"class": "form-control", "placeholder": "Муж/жен"}),
+    contactPersonEmail = forms.EmailField(widget=forms.EmailInput(attrs={"class": "form-control", "placeholder": "Email..."}),
                              required=True)
 
 
