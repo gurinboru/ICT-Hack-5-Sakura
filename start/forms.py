521baf01264bc841dbaef7ca12b6c13ed6fc8f76
slavamarcin <slavamarcin@yandex.ru>
2022-12-10 15:58:56 +0300

    [Feat] Add project form and view, change User Profile

--- start/forms.py
+++ start/forms.py
@@ -2,20 +2,44 @@ from django import forms
 from django.forms import NumberInput
 
 
-class changeForm(forms.Form):
+class changeStudentForm(forms.Form):
     first_name = forms.CharField(widget=forms.TextInput(attrs={"class":"form-control", "placeholder":"ФИО"}),required=True)
     last_name = forms.CharField(widget=forms.TextInput(attrs={"class":"form-control", "placeholder":"89........."}),required=True)
-    phone = forms.EmailField(widget=forms.EmailInput(attrs={"class":"form-control", "placeholder":"example@mail.ru"}),required=True)
-    email = forms.CharField(widget=forms.TextInput(attrs={"class":"form-control", "placeholder":"Муж/жен"}),required=True)
+    phone = forms.CharField(widget=forms.TextInput(attrs={"class":"form-control", "placeholder":"example@mail.ru"}),required=True)
+    email = forms.EmailField(widget=forms.EmailInput(attrs={"class":"form-control", "placeholder":"Муж/жен"}),required=True)
     username = forms.CharField(widget=forms.TextInput(attrs={"class":"form-control", "placeholder":"Разработчик"}),required=True)
     image = forms.ImageField(widget=forms.FileInput(attrs={"class":"form-control", "placeholder":"Фото"}),required=False)
-    tags = forms.DateField(widget=NumberInput(attrs={'type': 'date', "class":"form-control", "placeholder":"01.01.1999"}),required=True)
+    tags = forms.CharField(widget=forms.TextInput(attrs={ "class":"form-control", "placeholder":"01.01.1999"}),required=True)
     CV = forms.FileField(widget=forms.FileInput(attrs={"class":"form-control"}),required=False)
-    education = forms.DateField(widget=NumberInput(attrs={'type': 'date', "class": "form-control", "placeholder": "01.01.1999"}),
+    education = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control", "placeholder": "01.01.1999"}),
                        required=True)
-    department = forms.DateField(widget=NumberInput(attrs={'type': 'date', "class": "form-control", "placeholder": "01.01.1999"}),
+    department = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control", "placeholder": "01.01.1999"}),
                        required=True)
-    hardskill_softskill = forms.DateField(widget=NumberInput(attrs={'type': 'date', "class": "form-control", "placeholder": "01.01.1999"}),
+    hardskill_softskill = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control", "placeholder": "01.01.1999"}),
                        required=True)
-    experience = forms.DateField(widget=NumberInput(attrs={'type': 'date', "class": "form-control", "placeholder": "01.01.1999"}),
+    experience = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control", "placeholder": "01.01.1999"}),
                        required=True)
+
+class addProjectForm(forms.Form):
+    name = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control", "placeholder": "01.01.1999"}),
+                       required=True)
+    definitions = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control", "placeholder": "01.01.1999"}),
+                       required=True)
+    budjet = forms.IntegerField(widget=forms.NumberInput(attrs={"class": "form-control", "placeholder": "01.01.1999"}),
+                       required=True)
+    dedlines = forms.IntegerField(widget=forms.NumberInput(attrs={"class": "form-control", "placeholder": "01.01.1999"}),
+                       required=True)
+    positions = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control", "placeholder": "01.01.1999"}),
+                       required=True)
+    techDocument = forms.FileField(widget=forms.FileInput(attrs={"class": "form-control", "placeholder": "01.01.1999"}),
+                       required=True)
+    goalOfProject = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control", "placeholder": "01.01.1999"}),
+                       required=True)
+    background = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control", "placeholder": "01.01.1999"}),
+                       required=True)
+    result = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control", "placeholder": "01.01.1999"}),
+                       required=True)
+    criterias = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control", "placeholder": "01.01.1999"}),
+                       required=True)
+    tags = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control", "placeholder": "01.01.1999"}),
+                       required=False)
--- start/views.py
+++ start/views.py
@@ -1,6 +1,9 @@
 import os
+
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .models import *
+from .forms import *
 from django.shortcuts import render, redirect
 
 def start(request):
@@ -30,9 +33,92 @@ def getStudent(request,pk):
     return render(request, 'start/students.html',content)
 
 @login_required(login_url='/login')
-def cangeStudent(request,pk):
-    student = Student.objects.get(pk)
+def profile(request):
+    student = Student.objects.get(user = request.user)
     content = {
         "student" : student,
+        "user": request.user
     }
     return render(request, 'start/students.html',content)
+
+@login_required(login_url='/login')
+def cangeStudent(request):
+    user = request.user
+    student = Student.objects.get(user=user)
+    if request.method == "POST":
+            form = changeStudentForm(request.POST, request.FILES)
+            if form.is_valid():
+                cd = form.cleaned_data
+                user.first_name = cd["first_name"]
+                user.last_name = cd["last_name"]
+                user.username = cd["username"]
+                user.email = cd["email"]
+                user.phone = cd["phone"]
+                student.image = cd["image"]
+                student.tags = cd["tags"]
+                student.CV = cd["CV"]
+                student.education = cd["education"]
+                student.department = cd["department"]
+                student.hardskill_softskill = cd["hardskill_softskill"]
+                student.experience = cd["experience"]
+                user.save()
+                student.save()
+                return redirect('profile')
+    form = changeStudentForm(first_name=user.first_name, last_name=user.last_name, phone=user.phone,
+                             email=user.email, username=user.username, image=students.image, tags=students.tags,
+                             CV=students.CV, education=students.education, department=students.department,
+                             hardskill_softskill=students.hardskill_softskill,
+                             experience=students.experience, )
+    content = {
+        "form" : form,
+    }
+    return render(request, 'start/students.html',content)
+
+@login_required(login_url='/login')
+def projects(request):
+    projects = Project.objects.all()
+    content = {
+        "projects" : projects,
+    }
+    return render(request, 'start/students.html',content)
+
+@login_required(login_url='/login')
+def getProject(request,pk):
+    project = Project.objects.get(pk)
+    content = {
+        "project" : project,
+    }
+    return render(request, 'start/students.html',content)
+
+@login_required(login_url='/login')
+def addProject(request):
+    if Organization.objects.get(user = request.user).exists():
+        if request.method == "POST":
+            form = addProjectForm(request.POST, request.FILES)
+            if form.is_valid():
+                cd = form.cleaned_data
+                newProject = Project()
+                newProject.name = cd["name"]
+                newProject.definitions = cd["definitions"]
+                newProject.budjet = cd["budjet"]
+                newProject.dedlines = cd["dedlines"]
+                newProject.positions = cd["positions"]
+                newProject.techDocument = cd["techDocument"]
+                newProject.goalOfProject = cd["goalOfProject"]
+                newProject.background = cd["background"]
+                newProject.result = cd["result"]
+                newProject.criterias = cd["criterias"]
+                if cd["tags"]:
+                    newProject.tags = cd["tags"]
+                else:
+                    newProject.tags = None
+                newProject.save()
+                redirect('projects')
+        form = addProjectForm()
+        content = {
+            "form": form,
+        }
+        return render(request, 'start/addProject.html', content)
+    else:
+        messages.error(request,'Нет прав доступа')
+        return redirect('login')
