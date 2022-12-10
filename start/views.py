274220d7cf651f35bd90770db92a9fdcd8f85622
slavamarcin <slavamarcin@yandex.ru>
2022-12-10 16:55:47 +0300

    [Feat] Add diference type of userprofile

--- start/views.py
+++ start/views.py
@@ -56,57 +56,55 @@ def cangeProfile(request):
     user = request.user
     try:
         organization = Organization.objects.get(user = user)
-        if organization.exists():
-            if request.method == "POST":
-                    form = changeOrganizationForm(request.POST)
-                    if form.is_valid():
-                        cd = form.cleaned_data
-                        user.username = cd["username"]
-                        user.email = cd["email"]
-                        user.phone = cd["phone"]
-                        organization.name = cd['name']
-                        organization.INN = cd["INN"]
-                        organization.save()
-                        user.save()
-                        return redirect('profile')
-            form = changeOrganizationForm(phone=user.phone, name = organization.name, INN = organization.INN,
+        if request.method == "POST":
+                form = changeOrganizationForm(request.POST)
+                if form.is_valid():
+                    cd = form.cleaned_data
+                    user.username = cd["username"]
+                    user.email = cd["email"]
+                    user.phone = cd["phone"]
+                    organization.name = cd['name']
+                    organization.INN = cd["INN"]
+                    organization.save()
+                    user.save()
+                    return redirect('profile')
+        form = changeOrganizationForm(phone=user.phone, name = organization.name, INN = organization.INN,
                                      email=user.email, username=user.username)
-            content = {
-                "type": "organization",
-                "form" : form,
-            }
+        content = {
+            "type": "organization",
+            "form" : form,
+        }
     except Organization.DoesNotExist:
         try:
             student = Student.objects.get(user=user)
-            if student.exists():
-                if request.method == "POST":
-                        form = changeStudentForm(request.POST, request.FILES)
-                        if form.is_valid():
-                            cd = form.cleaned_data
-                            user.first_name = cd["first_name"]
-                            user.last_name = cd["last_name"]
-                            user.username = cd["username"]
-                            user.email = cd["email"]
-                            user.phone = cd["phone"]
-                            student.image = cd["image"]
-                            student.tags = cd["tags"]
-                            student.CV = cd["CV"]
-                            student.education = cd["education"]
-                            student.department = cd["department"]
-                            student.hardskill_softskill = cd["hardskill_softskill"]
-                            student.experience = cd["experience"]
-                            user.save()
-                            student.save()
-                            return redirect('profile')
-                form = changeStudentForm(first_name=user.first_name, last_name=user.last_name, phone=user.phone,
+            if request.method == "POST":
+                form = changeStudentForm(request.POST, request.FILES)
+                if form.is_valid():
+                    cd = form.cleaned_data
+                    user.first_name = cd["first_name"]
+                    user.last_name = cd["last_name"]
+                    user.username = cd["username"]
+                    user.email = cd["email"]
+                    user.phone = cd["phone"]
+                    student.image = cd["image"]
+                    student.tags = cd["tags"]
+                    student.CV = cd["CV"]
+                    student.education = cd["education"]
+                    student.department = cd["department"]
+                    student.hardskill_softskill = cd["hardskill_softskill"]
+                    student.experience = cd["experience"]
+                    user.save()
+                    student.save()
+                    return redirect('profile')
+            form = changeStudentForm(first_name=user.first_name, last_name=user.last_name, phone=user.phone,
                                          email=user.email, username=user.username, image=students.image, tags=students.tags,
                                          CV=students.CV, education=students.education, department=students.department,
                                          hardskill_softskill=students.hardskill_softskill,
                                          experience=students.experience )
-                content = {
-                    "type": "student",
-                    "form" : form,
-                }
+            content = {
+                "type": "student",
+                "form" : form,
+            }
         except Student.DoesNotExist:
             redirect('404')
     return render(request, 'start/students.html',content)
@@ -131,37 +129,36 @@ def getProject(request,pk):
 def addProject(request):
     try:
         organization = Organization.objects.get(user = request.user)
-        if organization.exists():
-            if request.method == "POST":
-                form = addProjectForm(request.POST, request.FILES)
-                if form.is_valid():
-                    cd = form.cleaned_data
-                    newContactPerson = ContactPerson(name = cd["contactPersonName"],email = cd["contactPersonEmail"], phone = cd["contactPersonPhone"]).save()
-                    newProject = Project()
-                    newProject.name = cd["name"]
-                    newProject.definitions = cd["definitions"]
-                    newProject.budjet = cd["budjet"]
-                    newProject.dedlines = cd["dedlines"]
-                    newProject.positions = cd["positions"]
-                    newProject.techDocument = cd["techDocument"]
-                    newProject.goalOfProject = cd["goalOfProject"]
-                    newProject.background = cd["background"]
-                    newProject.result = cd["result"]
-                    newProject.criterias = cd["criterias"]
-                    newProject.organization = organization
-                    newProject.contactPerson = newContactPerson
-                    newProject.save()
-                    if cd["tags"]:
-                        newProject.tags = cd["tags"]
-                    else:
-                        newProject.tags = None
-                    newProject.save()
-                    redirect('projects')
-            form = addProjectForm()
-            content = {
-                "form": form,
-            }
-            return render(request, 'start/addProject.html', content)
+        if request.method == "POST":
+            form = addProjectForm(request.POST, request.FILES)
+            if form.is_valid():
+                cd = form.cleaned_data
+                newContactPerson = ContactPerson(name = cd["contactPersonName"],email = cd["contactPersonEmail"], phone = cd["contactPersonPhone"]).save()
+                newProject = Project()
+                newProject.name = cd["name"]
+                newProject.definitions = cd["definitions"]
+                newProject.budjet = cd["budjet"]
+                newProject.dedlines = cd["dedlines"]
+                newProject.positions = cd["positions"]
+                newProject.techDocument = cd["techDocument"]
+                newProject.goalOfProject = cd["goalOfProject"]
+                newProject.background = cd["background"]
+                newProject.result = cd["result"]
+                newProject.criterias = cd["criterias"]
+                newProject.organization = organization
+                newProject.contactPerson = newContactPerson
+                newProject.save()
+                if cd["tags"]:
+                    newProject.tags = cd["tags"]
+                else:
+                    newProject.tags = None
+                newProject.save()
+                redirect('projects')
+        form = addProjectForm()
+        content = {
+            "form": form,
+        }
+        return render(request, 'start/addProject.html', content)
     except Organization.DoesNotExist:
         messages.error(request,'Нет прав доступа')
         return redirect('login')
