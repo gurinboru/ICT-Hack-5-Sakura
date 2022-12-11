96b27fa5f99257ce6538544aaf39b4cac4402b62
UntilaAA <untila.a.a@mail.ru>
2022-12-11 04:49:11 +0300

    [Fix]: updated urls

--- start/urls.py
+++ start/urls.py
@@ -12,5 +12,6 @@ urlpatterns = [
     path('profile', views.cangeProfile, name='profile'),
     path('rialtos', views.rialtos, name='rialtos'),
     path('currialto/<int:pk>', views.getRialto, name='currialto'),
+    path('curstudent/get_cv/<int:pk>',views.get_cv),
     path('currialto/get_presentation/<int:pk>',views.get_presentation),
 ]
